50fbfec4f5ce0545e244a6f17da3ac7b49f95acf
inggan <k6551558@gmail.com>
2019-08-07 14:17:12 +0900

    Updates

---
+++ precache-manifest.2b766c19af72967d06f8a3a7813bb336.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "320d62f092e32585e6ff30302a25485c",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "fdec285d9346a96a4292",
+    "url": "/TodoList/static/css/main.c67babfa.chunk.css"
+  },
+  {
+    "revision": "c18f8b2a7009a879e192",
+    "url": "/TodoList/static/js/2.fe6f4c48.chunk.js"
+  },
+  {
+    "revision": "fdec285d9346a96a4292",
+    "url": "/TodoList/static/js/main.9df2a70b.chunk.js"
+  },
+  {
+    "revision": "23ef1ee30c4a1e2cab36",
+    "url": "/TodoList/static/js/runtime~main.90b1048d.js"
+  }
+]);
